81e2a2bfe3f6953c6be08aee361aafeb7bbe105a
ever.zet <ever.zet@gmail.com>
2010-02-28 16:00:35 +0200

    MIT License added

--- Controller/AppController.m
+++ Controller/AppController.m
@@ -1,3 +1,29 @@
+/*
+ 
+ The MIT License
+ 
+ Copyright (c) 2009-2010 Konstantin Kudryashov <ever.zet@gmail.com>
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ 
+ */
+
 #import "AppController.h"
 
 @implementation AppController
@@ -11,7 +37,7 @@
   // Setup EZMilk & AppPreferences lib
   rtmApi = [[EZMilk alloc] initWithApiKey:[RTMKeys apiKey]
                              andApiSecret:[RTMKeys apiSecret]];
-  preferences = [[AppPreferences alloc] init];
+  prefHolder = [[AppPreferences alloc] init];
 
   // Setup DB & retrieve data
   [self initDatabase];
@@ -60,7 +86,7 @@
   [quickEntryController release];
   [preferencesController release];
 
-  [preferences release];
+  [prefHolder release];
   [rtmApi release];
 
   [super dealloc];
@@ -173,7 +199,7 @@
   {
     [preferencesController release];
   }
-  preferencesController = [[PreferencesController alloc] initWithPreferences: preferences
+  preferencesController = [[PreferencesController alloc] initWithPreferences: prefHolder
                                                              andRtmConnector: rtmApi];
   [preferencesController showWindow:self];
 }
@@ -235,7 +261,7 @@
   while (true)
   {
     NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
-    [rtmApi setToken:[preferences rtmToken]];
+    [rtmApi setToken:[prefHolder rtmToken]];
 
     if ([rtmApi token])
     {
@@ -245,7 +271,7 @@
     }
 
     [pool release];
-    [NSThread sleepForTimeInterval:[preferences rtmSyncInterval]];
+    [NSThread sleepForTimeInterval:[prefHolder rtmSyncInterval]];
   }
 }
 
